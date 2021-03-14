31edcda3e6f60eca5f990c113c7cbb5bc658eb2a
poinionq <ponionq@gmail.com>
2021-03-14 22:55:08 +0900

    new clone

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from "./video_item.module.css";
-const Videoitem = ({video}) => {
+const Videoitem = ({video,video:{snippet},onVideoClick, display}) => {
 
     // 반복하는게 싫다면
     // ({video}) ---props.video 축약
     // 이름을 변경 
     // ({video:itemVidoe})---이름 변경
-
+    const displayType = display == 'list'  ?  styles.list : styles.grid;
     return (
-        <li className={styles.item}>
+        <li className={`${styles.item} ${displayType}`} onClick={() => onVideoClick(video)}>
             <div>
                 <img src={video.snippet.thumbnails.default.url} alt="이미지"/>
                 <p>{video.snippet.title}</p>
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -3,10 +3,10 @@ import Videoitem from '../video_item/video_item';
 import styles from './video_list.module.css';
 
 
-const VideoList = props => (
-  <ul className={styles.ul}>
-    {props.videos.map(video => (
-      <Videoitem key={video.id} video={video} />
+const VideoList = ({videos,onVideoClick, display}) => (
+  <ul className={styles.videos}>
+    {videos.map(video => (
+      <Videoitem key={video.id} video={video} onVideoClick={onVideoClick} display={display}/>
     ))}
   </ul>
 );
